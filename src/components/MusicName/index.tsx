11a28e7e4347a457b1753eb70e4bfb5112543e78
talo007 <mrliu819@foxmail.com>
2021-05-26 15:05:12 +0800

    名字长度适配

--- src/components/MusicName/index.tsx
+++ src/components/MusicName/index.tsx
@@ -2,7 +2,7 @@
  * @Author: 刘玉田
  * @Date: 2021-05-25 15:15:43
  * @Last Modified by: 刘玉田
- * @Last Modified time: 2021-05-26 14:35:26
+ * @Last Modified time: 2021-05-26 15:04:37
  * 音乐名字处理
  */
 
@@ -13,7 +13,7 @@ import { mySubString } from '../../util'
 const MusicName: FC<{ name: string; alia: string[] }> = ({ name, alia }) => {
   return (
     <div className="music-name">
-      {name} {' '}
+      {mySubString(name, 35)} {' '}
       {name.length < 10 && (
         <span className="alia">
           {alia[0] &&
