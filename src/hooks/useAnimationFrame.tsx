b88166137040752a56522e48c3009ce0e4fd1bae
talo007 <mrliu819@foxmail.com>
2021-05-26 18:27:43 +0800

    添加合理注释

--- src/hooks/useAnimationFrame.tsx
+++ src/hooks/useAnimationFrame.tsx
@@ -1,5 +1,12 @@
 import { useEffect, useRef } from 'react';
 
+/**
+ * 使用自定义 hook 可以解决直接在代码中使用 
+ * requestAnimationFrame 回调函数无法读取 state 的情况
+ *
+ * @param {Function} callback 回调函数
+ * @param {boolean} runing 是否执行
+ */
 function useAnimationFrame(callback: Function, runing: boolean) {
   const cb = useRef<Function>(callback);
   const animationID = useRef(0);
