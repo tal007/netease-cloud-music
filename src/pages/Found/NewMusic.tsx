7dd509089801ddc18c0849d412015f8f8287b2ba
e <mrliu819@foxmail.com>
2021-06-05 11:42:44 +0800

    style: 样式修改

--- src/pages/Found/NewMusic.tsx
+++ src/pages/Found/NewMusic.tsx
@@ -2,19 +2,18 @@
  * @Author: 刘玉田
  * @Date: 2021-05-24 14:28:41
  * @Last Modified by: 刘玉田
- * @Last Modified time: 2021-05-28 13:47:45
+ * @Last Modified time: 2021-06-05 11:39:48
  * 新歌速递
  */
 
-import { FC, useState, useEffect } from 'react';
-import { Row, Skeleton } from 'antd'
+import { FC, useState, useEffect } from "react";
+import { Row, Skeleton } from "antd";
 
-import { fillNumber } from '../../util';
-import EntryTitle from '../../components/EntryTitle/index';
-import Loading from '../../components/Loading/index'
-import useUrlLoader from '../../hooks/useURLLoader';
-import NewMusicItem from '../../components/MusicItem/NewMusicItem';
-
+import { fillNumber } from "../../util";
+import EntryTitle from "../../components/EntryTitle/index";
+import Loading from "../../components/Loading/index";
+import useUrlLoader from "../../hooks/useURLLoader";
+import NewMusicItem from "../../components/MusicItem/NewMusicItem";
 
 interface MusicItemListResponse {
   data: MusicItem[];
@@ -27,24 +26,25 @@ const NewMusic: FC = () => {
   useEffect(() => {
     // limt 最大30
     // ajax<MusicItemListResponse>('/personalized/newsong?limit=10', 'GET')
-    ajax<MusicItemListResponse>('/top/song?type=0', 'GET')
+    ajax<MusicItemListResponse>("/top/song?type=0", "GET")
       .then((response) => {
-        const res = response.data.slice(0, 10)
+        const res = response.data.slice(0, 10);
         setNewMusicList(res);
       })
       .catch((err) => {});
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
-  if (loading) return (
-    <Loading showIcon={false}>
-      <Skeleton avatar paragraph={{ rows: 2 }} />
-    </Loading>
-  )
+  if (loading)
+    return (
+      <Loading showIcon={false}>
+        <Skeleton avatar paragraph={{ rows: 2 }} />
+      </Loading>
+    );
 
   return (
     <div className="app-found-new-music">
-      <EntryTitle titleName="新歌速递" to="/new-music"/>
+      <EntryTitle titleName="新歌速递" to="/new-music" />
       <Row gutter={[30, 30]} className="app-found-new-music-list">
         {newMusicList.map((music, index) => {
           return (
